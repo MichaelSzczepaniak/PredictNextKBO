31c1b769fcb2b959c8b6637e30aa60bc01dfd244
Michael Szczepaniak <mszczepaniak@pobox.com>
2016-05-30 14:21:41 -0600

    PreprocessEDA.Rmd - Clarify project scope, refactor getFileLines

--- modeldev/ksandbox.R
+++ modeldev/ksandbox.R
@@ -1,24 +1,18 @@
 rm(list = ls())
 source("Katz.R")
 
-trigs <- getNgramTables(3)  # for little test corpus
-unigrams <- getNgramTables(1)
-# getTrigramWinA('fry_the', trigs) # empty char vector
-wInA <- getOTTWinA('sell_the', trigs)
-wInB <- getUTTWinB('sell_the', trigs)
-big <- getNgramTables(2, prefixFilter = 'sell_the')
-alphaTrig <- getAlphaTrigram(0.5, trigs, big)
+predictNextWord <- function() {
+    gamma2=0.7; gamma3=0.7
+    obsTrigrams <- calc.qBO.trigramsA(gamma3, bigramPrefix=bigPre, trigrams=trigs)
+    alphaBig <- getAlphaBigram(gamma2, bigrams=bigrs, unigram=unig)
+    qboBigs <- calc.qBO.bigramsB(bigDiscount=gamma2, bigramPrefix=bigPre,
+                                 trigrams=trigs, bigrams=bigrs, unigrams=unigs)
+    alphaTrig <- getAlphaTrigram(gamma3, trigs, bigr)
+    qBO.trigs.B <- calc.qBO.trigramB(gamma3, bigPre, trigs, bigrs, unigs, alphaTrig)
+    all_trigrams <- rbind(obsTrigrams, qBO.trigs.B)
+    all_trigrams
+    predict_trigram <- all_trigrams[which.max(all_trigrams$prob),]
+    predict_trigram
+}
 
-unigram <- getNgramTables(1, prefixFilter = 'the')
-alphaBig <- getAlphaBigram(0.5, bigs, unig)  # 1/8 = 0.125
 
-bigDiscount=0.5; bigramPrefix='sell_the'
-trigs=getNgramTables(3)
-unobsTrigs=getUnobsTrigs('sell_the', trigs)
-bigs=getNgramTables(2)
-unigs=getNgramTables(1)
-unig=getNgramTables(1, prefixFilter = 'the')
-
-x <- calc.qBO.bigramsB(bigDiscount=bigDiscount, bigramPrefix=bigramPrefix,
-                       trigrams=trigs, bigrams=bigs, unigrams=unigs,
-                       unigram=unig)
