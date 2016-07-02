e81d6ff934afa5a9f51d59e72b1249865ce36768
Michael Szczepaniak <mszczepaniak@pobox.com>
2016-07-02 07:51:46 -0600

    mdsandbox.R - add file that makes calls to ngram generation and frequency table merging functions

---
+++ modeldev/mdsandbox.R
@@ -0,0 +1,22 @@
+
+
+ddir <- file.path("D:", "Dropbox", "sw_dev", "projects", "PredictNextKBO",
+                  "data", "en_US", "ngrams")
+# sort by freq (primary) then by ngram (secondary)
+unigrams.blogs.raw <- arrange(unigrams.blogs.raw, freq, ngram)
+unigrams.news.raw <- arrange(unigrams.news.raw, freq, ngram)
+unigrams.twitter.raw <- arrange(unigrams.twitter.raw, freq, ngram)
+# merge tables
+
+# rm(list = ls())
+source('Ngrams.R')
+merged.unigrams.raw <- mergeFreqTables(unigrams.blogs.raw, unigrams.news.raw, 1000, TRUE)
+merged.unigrams.raw <- arrange(merged.unigrams.raw, frequency, ngram)
+write.csv(merged.unigrams.raw, sprintf("%s%s", ddir, "ngrams/merged.blogs.news.csv"), row.names=FALSE)
+
+merged2.unigrams.raw <- mergeFreqTables(merged.unigrams.raw, unigrams.twitter.raw, 10000, TRUE)
+merged2.unigrams.raw <- arrange(merged2.unigrams.raw, freq, ngram)
+write.csv(merged2.unigrams.raw, sprintf("%s%s", ddir, "ngrams/merged.all.raw.csv"), row.names=FALSE)
+
+# get unigram singletons
+unigSingles.blogs <- unigrams.blogs.raw[unigrams.blogs.raw$freq==1,]
