02af86be8e020bfc330fb05fff014a642b81b346
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-07-11 23:22:45 -0600

    mdsandbox.R - add code to generate bigram tables accounting for USIN tokens

--- modeldev/mdsandbox.R
+++ modeldev/mdsandbox.R
@@ -30,4 +30,24 @@ breaking.indices <- getBreakingIndices(filePath)
 breakWritePartdFiles()
 singletons <- loadIgnoreParts()
 
+# build bigram freq tables for each corpus type
+rm(list = ls())
+source('Ngrams.R')
+loadLibs()
+input.postfix <- ".10fixeos.txt"
+input.prefixes <- c("en_US.blogs.train", "en_US.news.train", "en_US.twitter.train")
+input.paths <- sprintf("%s%s%s", ddir, input.prefixes, input.postfix)
+output.bigram.paths <- sprintf("%s%s%s", ddir, input.prefixes, ".12bigrams.csv")
+output.trigram.paths <- sprintf("%s%s%s", ddir, input.prefixes, ".13trigrams.csv")
 
+charvect <- read_lines(input.paths[1])
+bigrams.blogs <- getNgramTables(2, charvect, "USIN") # ignore unigram singletons
+write.csv(bigrams.blogs, output.bigram.paths[1], row.names = FALSE)
+
+charvect <- read_lines(input.paths[2])
+bigrams.news.raw <- getNgramTables(2, charvect, "USIN") # ignore unigram singletons
+write.csv(bigrams.news, output.bigram.paths[2], row.names = FALSE)
+
+charvect <- read_lines(input.paths[3])
+bigrams.twitter <- getNgramTables(2, charvect, "USIN") # ignore unigram singletons
+write.csv(bigrams.twitter, output.bigram.paths[3], row.names = FALSE)
