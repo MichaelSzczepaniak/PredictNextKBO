0a9bdd16351c3ec66bc1ba2534d451cadf8bb835
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-08-04 22:22:11 -0600

    ui.R - comment out Algorithm Path field in ui

--- predictnextkbo/ui.R
+++ predictnextkbo/ui.R
@@ -3,17 +3,17 @@
 modelChoices <- list("Blogs"=1, "News"=2, "Twitter"=3)
 
 fluidPage(
-    titlePanel(title="Predict Next Word (non-functional UI only)"),
+    titlePanel(title="Predict Next Word (dev UI)"),
     
     sidebarPanel(
         textInput('phrase', h4("Enter phrase:")),
         radioButtons('corpusToUSe', h4("Corpus"), modelChoices),
         sliderInput("bigDiscount",
                     label = h4("Bigram Discount"),
-                    min = 0.1, max = 1.9, value = 1.0, step = 0.1),
+                    min = 0.1, max = 1.9, value = 0.5, step = 0.1),
         sliderInput("trigDiscount",
                     label = h4("Trigram Discount"),
-                    min = 0.1, max = 1.9, value = 1.0, step = 0.1),
+                    min = 0.1, max = 1.9, value = 0.5, step = 0.1),
         submitButton("Predict next word of phrase")
     ),
     mainPanel(
@@ -21,8 +21,8 @@ fluidPage(
         verbatimTextOutput("sPredictionSettings"),
         h4('Last two words of input phrase:'),
         verbatimTextOutput("sInputBigram"),
-        h4('Algorithm Path:'),
-        verbatimTextOutput("sAlgoPath"),
+        # h4('Algorithm Path:'),
+        # verbatimTextOutput("sAlgoPath"),
         h4('Predicted Word:'),
         verbatimTextOutput("sPredictedWord"),
         plotOutput("pTop3Probs", width = 400, height = 300)
