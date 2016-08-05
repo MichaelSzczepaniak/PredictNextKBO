7891b198382ed3c3f38cdd02065b5479390f2bf8
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-08-04 22:20:53 -0600

    server.R - make inBigram a reactive var so it can be reused, source PredictNextWord.R instead of dev version

--- predictnextkbo/server.R
+++ predictnextkbo/server.R
@@ -1,14 +1,20 @@
-# source("PredictNextWord.R")
-source('PredictNextWordDevOnly.R')  ## DELETE THIS LINE AND USE ONE ABOVE WHEN READY!!!!
+source("PredictNextWord.R")
 
 shinyServer(
     function(input, output) {
+        
+        inBigram <- reactive({
+            getInputBigram(input$phrase)
+        })
+        
         output$sPredictionSettings <-
             renderPrint({getSettings(input$corpusToUSe, input$bigDiscount,
                                      input$trigDiscount)})
-        output$sInputBigram <- renderPrint({getInputBigram(input$phrase)})
-        output$sAlgoPath <- renderPrint({getPrediction(input$phrase)[2]})
-        output$sPredictedWord <- renderPrint({getPrediction(input$phrase)[1]})
+        
+        output$sInputBigram <- renderPrint(inBigram())
+        
+        output$sPredictedWord <- renderPrint({getPrediction(inBigram())})
+        
         output$pTop3Probs <- renderPlot({
             getPlot()
         })
