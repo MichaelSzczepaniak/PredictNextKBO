96437db594316cd222175e446fcc09726149fdf7
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-09-01 10:30:51 -0600

    PredictNextWord.R - remove references to paths to development data, remove referenced to ngram table reads that are outside the getTopNPredictions function, remove input filtering code which will be added back later

--- predictnextkbo/PredictNextWord.R
+++ predictnextkbo/PredictNextWord.R
@@ -1,12 +1,8 @@
 #
 source('Katz.R')
 options(stringsAsFactors = FALSE)  # strings are what we are operating on...
-# Use next 3 lines for development and troubleshooting
-# uniPaths <- "../data/ltc1_unigrams.csv"
-# bigPaths <- "../data/ltc1_bigrams.csv"
-# triPaths <- "../data/ltc1_trigrams.csv"
 
-# Next 9 lines are paths to corpus ngram tables to use for live deployement
+# Setup paths to the ngram frequency data used by the model
 uniPaths <- c("./data/en_US.blogs.train.12unigrams.nosins.csv",
               "./data/en_US.news.train.12unigrams.nosins.csv",
               "./data/en_US.twitter.train.12unigrams.nosins.csv")
@@ -16,15 +12,6 @@ bigPaths <- c("./data/en_US.blogs.train.13bigrams.nosins.csv",
 triPaths <- c("./data/en_US.blogs.train.14trigrams.nosins.csv",
              "./data/en_US.news.train.14trigrams.nosins.csv",
              "./data/en_US.twitter.train.14trigrams.nosins.csv")
-
-# default parameters for bigram and trigram discount rates
-# gamma2 <- 0.5
-# gamma3 <- 0.5
-
-# read n-gram tables upfront so they can be passed to the Katz.R functions
-# unigrams <- read.csv(uniPaths[1])
-# bigrams <- read.csv(bigPaths[1])
-# trigrams <- read.csv(triPaths[1])
 
 ## Gets the user settings to use for the model prediction, sets model parameters
 ## and loads the ngram frequency tables corresponding to the corpus to use
@@ -66,8 +53,7 @@ getInputBigram <- function(inputPhrase) {
 ##              1 for blogs, 2 for news, 3 for twitter
 ## gamma2 - bigram discount rate
 ## gamma3 - trigram discount rate
-getTopNPredictions <- function(bigPre, n=3, corp_index,
-                               gamma2=1.0, gamma3=1.0) {
+getTopNPredictions <- function(bigPre, n, corp_index, gamma2, gamma3) {
     # load unigram, bigram, and trigram tables corresponding to the corpus
     # selected by the user
     unigrams <- read.csv(uniPaths[corp_index])
@@ -111,130 +97,6 @@ getPrediction <- function(topPreds) {
     }
     
     return(prediction)
-}
-
-## TODO
-filterInput <- function(someText) {
-    # st <- ""
-    # if(nchar(someText) > 1) {
-    #     st <- preProfFilter(someText)
-    #     proflist <- readLines("./data/profanity.revised.txt")
-    #     st <- removeProfanity(proflist, st)
-    #     st <- postProfClean(st)
-    # }
-    # 
-    # return(st)
-    
-    return(someText)
-}
-
-## Replace chars similar to single quotes with simple ascii single quote chars
-## and then removes anything not a word character or character needed to create
-## one of the profanity words/phrases in the profanity list.
-preProfFilter <- function(samp, perl.flag=TRUE, is.news=FALSE) {
-    # news file has char's that break gsub, so convert to ASCII and remove NA's
-    if(is.news) {
-        samp <- iconv(samp, from="UTF-8", to="ASCII")
-        samp <- samp[-which(is.na(samp))]
-    }
-    # Handle right single quotes: 387317 instances in the blog file
-    # http://stackoverflow.com/questions/2477452/%C3%A2%E2%82%AC%E2%84%A2-showing-on-page-instead-of
-    samp <- gsub("(\xE2\x80\x99)", "'", samp, perl=perl.flag)
-    # Handle other chars that are like single quotes:
-    samp <- gsub("[\U0027\U00B4\U0092\U0060\U02BB\U02BC\U2018\U2019]",
-                 "'", samp, perl=perl.flag)
-    # Remove chars that can't be used to create profanity
-    samp <- gsub("[^ A-Za-z0-9.!'_*+<>&@#()$\\^\\[\\]\\-]", "", samp, perl=perl.flag)
-    return(samp)
-}
-
-## Converts all the text in flines to lower case (because profanity list is in lower case)
-## and then removes all profanity in the flines character vector before returning it.
-## Precondition - preProfFilter has been run on flines
-removeProfanity <- function(profList, samp) {
-    samp <- tolower(samp)  # profanity list is in lower case
-    corp.tmp <- Corpus(VectorSource(samp))
-    corp.tmp <- tm_map(corp.tmp, removeWords, profList)
-    samp <- corp.tmp[[1]]$content
-    # remove most variants of the all-time favorite profanity word which
-    # weren't caught above
-    samp <- gsub("(f+u+c+k+)", "", samp, ignore.case=TRUE, perl=TRUE)
-    return(samp)
-}
-
-## Removes URLs and anything not a word, space, or basic punctuation character
-## such as ?.!,:'- in a somewhat intelligent manner.
-postProfClean <- function(samp, is.news=TRUE) {
-    # Build regex to remove URLs. No shorthand character classes in R,
-    # so need to create by hand
-    wordChars <- "A-Za-z0-9_\\-"
-    urlRegex <- sprintf("%s%s%s", "(http|https)(://)?[", wordChars, "]+")
-    urlRegex <- sprintf("%s%s%s%s", urlRegex, "(.[", wordChars, "]+)+")
-    urlRegex <- sprintf("%s%s%s%s", urlRegex, "[", wordChars, ".,@?^=%&:/~\\+#]*")
-    # urlRegex <- "(http|https)://[\w\-_]+(\.[\w\-_]+)+[\w\-.,@?^=%&:/~\\+#]*"
-    samp <- gsub(urlRegex, "", samp, perl=TRUE)
-    if(!is.news) { samp <- nonNewsPostProfClean(samp) }
-    # remove anything that's not an alpha, digit basic punctuation char
-    # will replace digits with NUM later
-    samp <- gsub("[^A-Za-z0-9?.!,:'\\-]", " ", samp, perl=TRUE)
-    samp <- gsub("( ){2,}", " ", samp, perl=TRUE)  # replace >=2 spaces w/single space
-    samp <- gsub("^( . )", " ", samp, perl=TRUE)
-    samp <- gsub("^( ){1,}", "", samp, perl=TRUE)  # remove leading spaces
-    samp <- gsub("[ ]{1,}$", "", samp, perl=TRUE)  # remove trailing spaces
-    # remove non-alpha char's that start sentences
-    samp <- gsub("^[^A-Za-z]+", "", samp)
-    # make lines that don't end in . ! or ? empty so they'll be removed later
-    # samp <- gsub("^.*[^.!?]$", "", samp)
-    # replace non-word-period by just period
-    samp <- gsub("([^A-Za-z0-9]+.)$", ".", samp)
-    # remove lines that don't have any alpha characters
-    samp <- gsub("^[^A-Za-z]+$", "", samp, perl=TRUE)
-    # remove empty lines
-    samp <- samp[which(samp  != "")]
-    # replace 2 or more spaces with a single space
-    samp <- gsub("[ ]{2,}", " ", samp, perl=TRUE)
-    # normalize text to lower case
-    samp <- tolower(samp)
-    # replace sequences of digits by NUM token: after lower case to keep
-    # this special token UPPER CASE in the processed file
-    samp <- gsub("[0-9]+", "NUM", samp)
-    # remove terminating chars like punctuation etc.
-    samp <- gsub("([^a-z]+)$", "", samp, ignore.case=TRUE, perl=TRUE)
-    
-    return(samp)
-}
-
-## Does additional cleaning for twitter and blog files
-nonNewsPostProfClean <- function(flines) {
-    cat("start nonNewsPostProfClean:", as.character(Sys.time()), "\n")
-    # remove all lines that don't contain alpha char's
-    flines.edit <- gsub("^[^a-z]$", "", flines, ignore.case=T, perl=T)
-    flines.edit <- flines.edit[which(flines.edit != "")]
-    # remove most variants of the all-time favorite profanity word which
-    # weren't caught by profanity filter
-    flines.edit <- gsub("(f+u+c+k+)", "", flines.edit,
-                        ignore.case=TRUE, perl=TRUE)
-    # remove 'omg' and 'wow'
-    flines.edit <- gsub("(omg|o m g)", "", flines.edit, ignore.case=TRUE, perl=TRUE)
-    flines.edit <- gsub("(wow|w o w)", "", flines.edit, ignore.case=TRUE, perl=TRUE)
-    # remove trailing periods
-    flines.edit <- gsub("^[.]+", "", flines.edit, ignore.case=TRUE, perl=TRUE)
-    # remove embedded periods
-    flines.edit <- gsub("([a-z]+)([.]+)([a-z]+)", "\\1 \\3", flines.edit,
-                        ignore.case=TRUE, perl=TRUE)
-    # replace space-period-space with just space
-    flines.edit <- gsub(" [.] ", " ", flines.edit, ignore.case=TRUE, perl=TRUE)
-    # remove periods assoc'd w/ morning and evening time abbrev's
-    flines.edit <- gsub("(a m.)", "am", flines.edit,
-                        ignore.case=TRUE, perl=TRUE)
-    flines.edit <- gsub("(p m.)", "pm", flines.edit,
-                        ignore.case=TRUE, perl=TRUE)
-    # remove remaining www instances
-    flines.edit <- gsub("[a-z][.][a-z]", " ", flines.edit,
-                        ignore.case=TRUE, perl=TRUE)
-    cat("finish nonNewsPostProfClean:", as.character(Sys.time()), "\n")
-    
-    return(flines.edit)
 }
 
 getBigramsStartingWithChars <- function(wrd1=NULL, wrd2=NULL,
