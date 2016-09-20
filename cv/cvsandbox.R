73a5a34b51d6f28a64ba4365982cd029193376ed
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-09-20 12:38:33 -0600

    cvsandbox.R - revise dev code to output topn gamma pairs per itr fold

--- cv/cvsandbox.R
+++ cv/cvsandbox.R
@@ -12,11 +12,17 @@ corpus_data <- c("https://www.dropbox.com/s/9dx3oo1w5uf8n1t/en_US.blogs.train.8p
                  "https://www.dropbox.com/s/54cvi36161y6pvk/en_US.news.train.8posteos.txt?dl=1",
                  "https://www.dropbox.com/s/6ayhavfnzs5lmqa/en_US.twitter.train.8posteos.txt?dl=1")
 
-corpus_lines <- read_lines(corpus_data[1])
+corpus_ <- read_lines(corpus_data[1])
 
 # ng_paths=c("https://www.dropbox.com/s/033qzeiggmcauo9/en_US.blogs.train.12unigrams.nosins.csv?dl=1",
 #            "https://www.dropbox.com/s/6cgqa487xb0srbt/en_US.blogs.train.13bigrams.nosins.csv?dl=1",
 #            "https://www.dropbox.com/s/z0rz707mt3da1h1/en_US.blogs.train.14trigrams.nosins.csv?dl=1")
+
+fold_paths <- c("D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_1blogs.txt",
+                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_2blogs.txt",
+                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_3blogs.txt",
+                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_4blogs.txt",
+                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_5blogs.txt")
 
 readFolds <- function(fold_paths) {
     folds <- vector("list", length(fold_paths))
@@ -29,27 +35,27 @@ readFolds <- function(fold_paths) {
     return(folds)
 }
 
-fold_paths <- c("D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_1.txt",
-                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_2.txt",
-                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_3.txt",
-                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_4.txt",
-                "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/fold_5.txt")
-
 default_folds <- readFolds(fold_paths)
 out_default <- "D:/Dropbox/sw_dev/projects/PredictNextKBO/cv/"
+topn=3; corpus_type="blogs"
 ggrid_start=1; folds=default_folds; fold_start=1; nitrs=200
 kfolds=5; out_dir=out_default; seed_val=719
 
-## Runs K-Fold CV on corpus_train and returns a list of best pairs of (gamma2,
-## gamma3) with the highest prediction accuracy for each validation fold
-runKfoldTrials <- function(corpus_train, gamma_grid, ggrid_start=1,
-                           folds=default_folds, fold_start=1, nitrs=200,
+## Runs K-Fold CV on corpus_lines and returns a data.frame of topn of best
+## pairs of (gamma2, gamma3) with the highest prediction accuracy for each
+## validation fold
+runKfoldTrials <- function(corpus_lines, gamma_grid, corpus_type="blogs",
+                           topn=3, ggrid_start=1, folds=default_folds,
+                           fold_start=1, nitrs=200,
                            kfolds=5, out_dir=out_default, seed_val=719) {
-    best_gammas <- data.frame(vfold=seq(1, 5), gamma2=rep(-1, 5),
-                              gamma3=rep(-1, 5), pred_acc=rep(-1, 5))
+    best_gammas <- data.frame(vfold=rep(1:kfolds, each=topn),
+                              rank=rep(1:topn, kfolds),
+                              gamma2=rep(-1, kfolds*topn),
+                              gamma3=rep(-1, kfolds*topn),
+                              pred_acc=rep(-1, kfolds*topn))
     set.seed(seed_val)  # set for reproducibility
-    # folds <- makeFolds(length(corpus_lines), kfolds) # writes fold indices
-    out_file <- paste0(out_dir, "cv_results.csv")
+    out_file <- paste0(out_dir, "cv_", corpus_type, "_", kfolds, "fold_",
+                       nitrs, ".csv")
     for(f in fold_start:length(folds)) {
         valid_fold_indices <- folds[[f]]
         valid_data <- corpus_lines[valid_fold_indices]
@@ -66,14 +72,14 @@ runKfoldTrials <- function(corpus_train, gamma_grid, ggrid_start=1,
         trigs <- getNgramTables(3, train_ngrams_data)  # ~ 8.5 mins
         trigs <- filter(trigs, freq > 1)
         
-        # This is the actual training steps.  Take nitrs lines in training set
-        # that weren't used to build n-gram tables and make predictions using 
-        # each (gamma2, gamma3) pair in gamma_grid
-        trigrams_to_predict <- getRandomTrigrams(train_gammas_data, nitrs)
-        # [[1]] accuracy, [[2]] gamma2, [[3]] gamma3
-        exp_results <- list(acc=as.numeric(seq(1, nrow(gamma_grid))),
-                            gamma2=as.numeric(seq(1, nrow(gamma_grid))),
-                            gamma3=as.numeric(seq(1, nrow(gamma_grid))))
+        # This is the actual training steps.  Take nitrs trigram samples from
+        # the training setthat weren't used to build n-gram tables and make
+        # predictions using each (gamma2, gamma3) pair in gamma_grid.
+        trigrams_to_predict <- getUniqueRandomTrigrams(train_gammas_data, nitrs)
+        # experiment results:
+        exp_results <- data.frame(gamma2=as.numeric(rep(-1, nrow(gamma_grid))),
+                                  gamma3=as.numeric(rep(-1, nrow(gamma_grid))),
+                                  acc=as.numeric(rep(-1, nrow(gamma_grid))))
         for(experiment in ggrid_start:nrow(gamma_grid)) {
             good_predictions <- 0
             gam2 <- gamma_grid$gamma2[experiment]
@@ -89,22 +95,25 @@ runKfoldTrials <- function(corpus_train, gamma_grid, ggrid_start=1,
                 good_predictions <- good_predictions + (target_word == top_pred)
             }
             accuracy <- good_predictions / nitrs
-            exp_results[[1]][experiment] <- accuracy
-            exp_results[[2]][experiment] <- gam2
-            exp_results[[3]][experiment] <- gam3
+            exp_results$acc[experiment] <- accuracy
+            exp_results$gamma2[experiment] <- gam2
+            exp_results$gamma3[experiment] <- gam3
             out_line <- sprintf("%s%s%s%s%s", gam2, ",",gam3, ",", accuracy)
             cat(out_line, ",", as.character(Sys.time()), "\n")
         }
-        acc_max_index <- which.max(exp_results[[1]])
-        best_gammas$vfold[f] <- f
-        best_gammas$gamma2[f] <- exp_results[[1]][acc_max_index]
-        best_gammas$gamma3[f] <- exp_results[[2]][acc_max_index]
-        best_gammas$pred_acc[f] <- exp_results[[3]][acc_max_index]
+        # get the topn most accurate predictions
+        exp_results <- arrange(exp_results, desc(acc))[1:topn,]
+        # load topn results
+        start_block <- ((f - 1) * topn) + 1
+        end_block <- f * topn
+        best_gammas$vfold[start_block:end_block] <- f
+        best_gammas$gamma2[start_block:end_block] <- exp_results$gamma2[1:topn]
+        best_gammas$gamma3[start_block:end_block] <- exp_results$gamma3[1:topn]
+        best_gammas$pred_acc[start_block:end_block] <- exp_results$acc[1:topn]
+        
         write.csv(best_gammas, out_file, row.names = FALSE)
         cat("results written to:", out_file, "\n")
     }
-    
-    
     
     return(best_gammas)
 }
